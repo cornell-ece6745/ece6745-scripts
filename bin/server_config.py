4fd3d6c37f7112e0e7c638e7bafc4d55c161120a
hawajkm <hawajkm@gmail.com>
2017-08-29 03:30:03 -0400

    [ece4750-scripts/bin] Server/client backend scripts
    Using TCP/IP for inter-process communication

---
+++ bin/server_config.py
@@ -0,0 +1,20 @@
+#=========================================================================
+# server_config
+#=========================================================================
+#
+# Author : Khalid Al-Hawaj
+# Date   : August 26, 2017
+
+class server_config():
+
+  def __init__( self ):
+    # Store the following configuration in a configuration file
+    # default to the following if nothing has been provided
+    self.server_port = 1024
+    self.utils_port  = 2048
+    self.server_addr = 'ecelinux-01.ece.cornell.edu'
+
+    self.superusers  = [ 'ka429', 'eu49' ]
+
+  def getSU( self ):
+    return self.superusers
