58b2116184d6c22a7fd16643ad3cc69af3bbfd8c
cb535 <cb535@cornell.edu>
2025-01-25 18:32:21 -0500

    move scripts for use in ece 6745

--- bin/setup-ece6745.sh
+++ bin/setup-ece6745.sh
@@ -1,7 +1,7 @@
 #=========================================================================
-# setup-ece2300.sh [-q]
+# setup-ece6745.sh [-q]
 #=========================================================================
-# This setup script is for ECE 2300. It will set various environment
+# This setup script is for ECE 6745. It will set various environment
 # variables every time it is run, but can also run some heavier weight
 # commands once when an account is first initialized.
 
@@ -60,7 +60,7 @@ function printn
 #-------------------------------------------------------------------------
 
 print ""
-print " Running ECE 2300 setup script"
+print " Running ECE 6745 setup script"
 
 #-------------------------------------------------------------------------
 # Auto setup
@@ -74,21 +74,21 @@ if [[ "${enable_auto_setup}" == "yes" ]]; then
   print "  - removing line to source setup script from .bashrc"
 
   rm -rf ${HOME}/.bashrc.bak
-  sed -i.bak -e '/# ECE2300 BEGIN SETUP/,/# ECE2300 END SETUP/d' ${HOME}/.bashrc
+  sed -i.bak -e '/# ECE6745 BEGIN SETUP/,/# ECE6745 END SETUP/d' ${HOME}/.bashrc
 
   print "  - adding line to source setup script to .bashrc"
-  echo "# ECE2300 BEGIN SETUP"                     >> ${HOME}/.bashrc
+  echo "# ECE6745 BEGIN SETUP"                     >> ${HOME}/.bashrc
   echo ""                                          >> ${HOME}/.bashrc
-  echo "source /classes/setup/setup-ece2300.sh -q" >> ${HOME}/.bashrc
+  echo "source /classes/setup/setup-ece6745.sh -q" >> ${HOME}/.bashrc
   echo ""                                          >> ${HOME}/.bashrc
-  echo "# ECE2300 END SETUP"                       >> ${HOME}/.bashrc
+  echo "# ECE6745 END SETUP"                       >> ${HOME}/.bashrc
 
   echo ""
   echo " NOTE: Your login script has been updated so that it will"
   echo " automatically source the setup script every time you log"
   echo " ecelinux. You can disable this behavior with this command:"
   echo ""
-  echo "  % source setup-ece2300.sh --disable-auto-setup"
+  echo "  % source setup-ece6745.sh --disable-auto-setup"
   echo ""
   echo " After disabling auto setup you will need manually source"
   echo " the setup script every time you want to work on the course"
@@ -101,7 +101,7 @@ if [[ "${disable_auto_setup}" == "yes" ]]; then
   print "  - removing line to source setup script from .bashrc"
 
   rm -rf ${HOME}/.bashrc.bak
-  sed -i.bak -e '/# ECE2300 BEGIN SETUP/,/# ECE2300 END SETUP/d' ${HOME}/.bashrc
+  sed -i.bak -e '/# ECE6745 BEGIN SETUP/,/# ECE6745 END SETUP/d' ${HOME}/.bashrc
 
   echo ""
   echo " NOTE: Your login script has been updated so that it will"
@@ -116,33 +116,15 @@ fi
 # Make sure this script is only sourced once
 #-------------------------------------------------------------------------
 
-if [[ "x${SETUP_ECE2300}" == "xyes" ]]; then
+if [[ "x${SETUP_ECE6745}" == "xyes" ]]; then
   print ""
-  print " It looks like you have already sourced the setup-ece2300.sh"
+  print " It looks like you have already sourced the setup-ece6745.sh"
   print " script, so we are not going to do any additional setup."
   print " You should be all set. If for some reason you were trying"
   print " to see the effect of an updated version of the setup script"
   print " then just log out, log back into ecelinux, and source the"
   print " setup script again."
   print ""
-  return
-fi
-
-#-------------------------------------------------------------------------
-# Make sure ece5745 script has not been sourced
-#-------------------------------------------------------------------------
-
-if [[ "x${SETUP_ECE5745}" == "xyes" ]]; then
-  echo ""
-  echo " It looks like you have already sourced the setup-ece5745.sh"
-  echo " script, but you can only source the setup-ece5745.sh script"
-  echo " _or_ the setup-ece2300.sh script ... not both! If you did"
-  echo " not explicitly source the setup-ece5745.sh script, then it"
-  echo " is probably being sourced automatically in your .bashrc"
-  echo " file. Open the $HOME/.bashrc file using geany or your"
-  echo " favorite text editor and remove any lines which source"
-  echo " setup-ece5745.sh. Then log out and log back into ecelinux."
-  echo ""
   return
 fi
 
@@ -171,15 +153,15 @@ fi
 # Set environment variable so we know this script has been sourced
 #-------------------------------------------------------------------------
 
-export SETUP_COURSE="ece2300"
-export SETUP_ECE2300="yes"
+export SETUP_COURSE="ece6745"
+export SETUP_ECE6745="yes"
 
 #-------------------------------------------------------------------------
 # Determine if we need to rerun initialization commands
 #-------------------------------------------------------------------------
 # Some of these commands only need to be done once, the very first time a
 # user logs into their account. To try and reduce overhead, we keep a
-# file in the users home directory called .setup-ece2300 which has a
+# file in the users home directory called .setup-ece6745 which has a
 # counter. If the last count is less than the count we are looking for,
 # then we update the count and rerun these initialization commands. This
 # way we can force users to rerun initialization commands by simply
@@ -188,8 +170,8 @@ export SETUP_ECE2300="yes"
 run_init_cmds_last_ver="?"
 run_init_cmds_curr_ver="2"
 run_init_cmds="yes"
-if [[ -f "${HOME}/.setup-ece2300" ]]; then
-  run_init_cmds_last_ver=$(tail -1 "${HOME}/.setup-ece2300")
+if [[ -f "${HOME}/.setup-ece6745" ]]; then
+  run_init_cmds_last_ver=$(tail -1 "${HOME}/.setup-ece6745")
   if [[ "${run_init_cmds_curr_ver}" == "${run_init_cmds_last_ver}" ]]; then
     run_init_cmds="no"
   fi
@@ -199,13 +181,13 @@ print "  - Determine init commands version number: ${run_init_cmds_last_ver}"
 
 if [[ "${run_init_cmds}" == "yes" ]]; then
   print "  - Initialization commands outdated, will be rerun"
-  echo "${run_init_cmds_curr_ver}" >> "${HOME}/.setup-ece2300"
+  echo "${run_init_cmds_curr_ver}" >> "${HOME}/.setup-ece6745"
 fi
 
 #-------------------------------------------------------------------------
 # Determine which platform we are running on
 #-------------------------------------------------------------------------
-# The ECE 2300 setup script is only meant to be run on ecelinux, so we
+# The ECE 6745 setup script is only meant to be run on ecelinux, so we
 # simply check that it is running on the correct servers and hard code
 # the ARCH environment variable.
 
@@ -217,7 +199,7 @@ esac
 
 if [[ ${server_class} == "unknown" ]]; then
   print ""
-  print " The ECE 2300 tools are not supported on this machine."
+  print " The ECE 6745 tools are not supported on this machine."
   print " The course is only supported on ecelinux machines."
   print ""
   return
@@ -227,27 +209,27 @@ fi
 # SSH Keys
 #-------------------------------------------------------------------------
 
-if [[ ! -f "${HOME}/.ssh/ece2300-github.pub" ]]; then
+if [[ ! -f "${HOME}/.ssh/ece6745-github.pub" ]]; then
 
   print "  - Setting up ssh keys for accessing github"
 
   netid=$(whoami)
-  ssh-keygen -q -N "" -t rsa -C "${netid}@cornell.edu" -f "${HOME}/.ssh/ece2300-github"
+  ssh-keygen -q -N "" -t rsa -C "${netid}@cornell.edu" -f "${HOME}/.ssh/ece6745-github"
 
   echo ""                                     >> "${HOME}/.ssh/config"
-  echo "# added by setup-ece2300.sh"          >> "${HOME}/.ssh/config"
+  echo "# added by setup-ece6745.sh"          >> "${HOME}/.ssh/config"
   echo "Host github.com"                      >> "${HOME}/.ssh/config"
-  echo "  IdentityFile ~/.ssh/ece2300-github" >> "${HOME}/.ssh/config"
+  echo "  IdentityFile ~/.ssh/ece6745-github" >> "${HOME}/.ssh/config"
   echo ""                                     >> "${HOME}/.ssh/config"
 
 fi
 
-if ! grep -q "# key/X11 forwarding added by setup-ece2300.sh" "${HOME}/.ssh/config"; then
+if ! grep -q "# key/X11 forwarding added by setup-ece6745.sh" "${HOME}/.ssh/config"; then
 
   print "  - Running ssh initialization to ensure key/X11 forwarding enabled"
 
   echo ""                          >> "${HOME}/.ssh/config"
-  echo "# key/X11 forwarding added by setup-ece2300.sh" >> "${HOME}/.ssh/config"
+  echo "# key/X11 forwarding added by setup-ece6745.sh" >> "${HOME}/.ssh/config"
   echo ""                          >> "${HOME}/.ssh/config"
   echo "Host *.ece.cornell.edu"    >> "${HOME}/.ssh/config"
   echo "  ForwardAgent  yes"       >> "${HOME}/.ssh/config"
@@ -269,18 +251,18 @@ print "  - Setting up ssh permissions"
 
 chmod 700 ~/.ssh
 chmod 600 ~/.ssh/config
-chmod 600 ~/.ssh/ece2300-github.pub
-chmod 600 ~/.ssh/ece2300-github
+chmod 600 ~/.ssh/ece6745-github.pub
+chmod 600 ~/.ssh/ece6745-github
 
 #-------------------------------------------------------------------------
 # Setup environment modules
 #-------------------------------------------------------------------------
 
-export ECE2300_INSTALL="/classes/ece2300/install"
-export PATH="${ECE2300_INSTALL}/pkgs/modules-5.4.0/bin:${PATH}"
-source "${ECE2300_INSTALL}/pkgs/modules-5.4.0/init/bash"
+export ECE6745_INSTALL="/classes/ece6745/install"
+export PATH="${ECE6745_INSTALL}/pkgs/modules-5.5.0/bin:${PATH}"
+source "${ECE6745_INSTALL}/pkgs/modules-5.5.0/init/bash"
 
-module use "${ECE2300_INSTALL}/modules"
+module use "${ECE6745_INSTALL}/modules"
 
 export MODULES_COLORS="hi=1:db=2:tr=2:se=2:er=91:wa=93:me=95:in=94:mp=1;94:di=94:al=96:va=93:sy=95:de=4:cm=92:aL=4:L=4;32:H=2:F=41:nF=43:S=46:sS=44:kL=30;48;5;109"
 export MODULES_COLLECTION_PIN_VERSION=1
@@ -295,17 +277,17 @@ function module_load
   module load $1
 }
 
-module_load ece2300-scripts/0.0
-module_load bash-completion/2.14.0
-module_load gcc/13.2.1
-module_load iverilog/12.0
-module_load verilator/5.026
-module_load gtkwave/3.3.120
-module_load verible/0.0-3756-gda9a0f8c
-module_load quartus-prime-lite/18.1.0
-module_load gh/2.56.0
-module_load python/3.11.9
-module_load venvs/py3.11.9-default
+# module_load ece6745-scripts/0.0
+# module_load bash-completion/2.14.0
+# module_load gcc/13.2.1
+# module_load iverilog/12.0
+# module_load verilator/5.026
+# module_load gtkwave/3.3.120
+# module_load verible/0.0-3756-gda9a0f8c
+# module_load quartus-prime-lite/18.1.0
+# module_load gh/2.56.0
+# module_load python/3.11.9
+# module_load venvs/py3.11.9-default
 
 #-------------------------------------------------------------------------
 # Git initialization commands
@@ -317,8 +299,8 @@ if [[ "${run_init_cmds}" == "yes" ]]; then
 
   # Include git config from the install directories
 
-  if [[ -d "${ECE2300_INSTALL}/pkgs/ece2300-scripts-0.0/etc/gitconfig.d" ]]; then
-    for file in $(find "${ECE2300_INSTALL}/pkgs/ece2300-scripts-0.0/etc/gitconfig.d" -type f); do
+  if [[ -d "${ECE6745_INSTALL}/pkgs/ece6745-scripts-0.0/etc/gitconfig.d" ]]; then
+    for file in $(find "${ECE6745_INSTALL}/pkgs/ece6745-scripts-0.0/etc/gitconfig.d" -type f); do
       git config --global include.path "${file}"
     done
   fi
@@ -348,7 +330,7 @@ export EDITOR="nano"
 
 print "  - Setting up prompt"
 
-export PS1="\[\e[1;34m\]ECE2300:\[\e[0m\] \[\e[1m\]\w\[\e[0m\] % "
+export PS1="\[\e[1;34m\]ECE6745:\[\e[0m\] \[\e[1m\]\w\[\e[0m\] % "
 export PROMPT_DIRTRIM=2
 
 #-------------------------------------------------------------------------
